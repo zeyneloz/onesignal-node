86e0fa24d073deba76666d5123b19ada7da02bb6
= <=>
2017-07-28 11:51:05 +0300

    test

--- test/test.js
+++ test/test.js
@@ -2,9 +2,3 @@
 
 var expect = require('chai').expect;
 var OneSignal = require('../lib');
-
-// first we need to create a client
-var myClient = new OneSignal.Client({
-    userAuthKey: 'XXXXXX',
-    app: { appAuthKey: 'XXXXX', appId: 'XXXXX' }
-});
